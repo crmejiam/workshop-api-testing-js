cbb4646405509f551e48ea4d69c84563782e32dc
Cristian Mejia <crmejiam@unal.edu.co>
2021-02-11 13:06:04 -0500

    Consuming POST and PATCH Methods... but it has a bug, when creating an issue, it shows the status code 410 GONE

---
+++ test/GithubApi.Issue.test.js
@@ -0,0 +1,39 @@
+// Commit: Consuming POST and PATCH Methods
+const agent = require('superagent');
+// const statusCode = require('http-status-codes');
+const { expect } = require('chai');
+
+const urlBase = 'https://api.github.com';
+let userInfo;
+let repository;
+
+describe('GitHub Api POST and PATCH Test', () => {
+  describe('Checking that there is at least one public repo', () => {
+    it('Then there should be at least one public repo', () => agent.get(`${urlBase}/user`)
+      .set('User-Agent', 'agent')
+      .auth('token', process.env.ACCESS_TOKEN)
+      .then((response) => {
+        userInfo = response.body;
+        expect(response.body.public_repos).to.be.above(0);
+      }));
+  });
+  describe('Choosing a random public repo and checking that it exists', () => {
+    it('Then the choosed repo should exist', () => agent.get(userInfo.repos_url)
+      .set('User-Agent', 'agent')
+      .auth('token', process.env.ACCESS_TOKEN)
+      .then((response) => {
+        [repository] = response.body;
+        expect(repository).to.not.equal(null);
+      }));
+  });
+  describe('Creating an issue', () => {
+    it('Then the issue should be created', () => agent.post(`${urlBase}/repos/${userInfo.login}/${repository.name}/issues`)
+      .send({ title: 'New issue created through GitHub Api' })
+      .set('User-Agent', 'agent')
+      .auth('token', process.env.ACCESS_TOKEN)
+      .then((response) => {
+        expect(response.body.title).to.equal('New issue created through GitHub Api');
+        expect(response.body).to.equal(null);
+      }));
+  });
+});
